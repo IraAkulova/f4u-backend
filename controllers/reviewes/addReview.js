0e668db7972676ae224035cb85903bf9529d5e3b
Ira Akulova <ira.akulova2019@gmail.com>
2023-07-06 12:38:22 +0300

    Update addReview.js

--- controllers/reviewes/addReview.js
+++ controllers/reviewes/addReview.js
@@ -7,13 +7,15 @@ const gravatar = require('gravatar');
 
 const create = async (req, res, next) => {
     const { name, email, phone, comment } = req.body;
-    const avatarURL = gravatar.url(email);
-
     const { error } = req.body;
     if (error) {
         throw HttpError(400, `Missing required name field`)
     }
-    const result = await Review.create({ name, email, phone, comment, avatarURL })
+    const avatarURL = gravatar.url(email);
+    const newReview = await Review.create({ ...req.body, avatarURL, cat: true});
+    // const newReview = await Review.create({ name, email, phone, comment, avatarURL });
+        console.log(newReview)
+    
     const addedComment = {
         to: 'ira.akulova2019@gmail.com',
         subject: 'User left a review',
@@ -24,7 +26,7 @@ const create = async (req, res, next) => {
     res.status(201).json({
         status: 'success',
         code: 201,
-        data: { review: result },
+        data: { review: newReview },
     })
 }
 
